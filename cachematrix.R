9d0f7c9c3d7c4911041610041686723252a7f7ac
himalibhatt <mshimali@gmail.com>
2015-06-20 00:56:36 +0530

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special "matrix", which is a list of four functions : set(), get(), setsolve() and getsolve().
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+
+  m <- NULL
+        
+set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+get <- function() x
+        
+setsolve <- function(solve) 
+m <<- solve
+        
+getsolve <- function() m
+        
+list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+
+## The following function calculates the inverse of the special "matrix" created with the above function.
+
+## It first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. 
+
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse of matrix in the cache via the setsolve function.
+
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+ 
+                               m <- x$getsolve()
+        
+                               if(!is.null(m)) {
+        
+                                                message("getting cache data")
+ 
+                                               return(m)
+      
+                                           } else {
+    
+                                                  message("calculating inverse")
+      
+                                                  data <- x$get()
+    
+                                                   m <- solve(data, ...)
+      
+                                                   x$setsolve(m)
+        m
+      
+                                             }
+
+
+
+
 }
+
+
